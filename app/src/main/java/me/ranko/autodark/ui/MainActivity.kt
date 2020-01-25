7ad1d2bf46fc8ca7ba28aa5e6a79458d134f827d
0ranko0P <ranko0p@outlook.com>
2020-01-25 23:35:59 +0800

    Notify user if boot receiver is unavailable

--- app/src/main/java/me/ranko/autodark/ui/MainActivity.kt
+++ app/src/main/java/me/ranko/autodark/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package me.ranko.autodark.ui
 
 import android.content.res.ColorStateList
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.Observable
@@ -11,12 +12,22 @@ import androidx.lifecycle.ViewModelProviders
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.snackbar.Snackbar
 import me.ranko.autodark.R
+import me.ranko.autodark.Receivers.DarkModeAlarmReceiver
+import me.ranko.autodark.Utils.ComponentUtil
 import me.ranko.autodark.Utils.ViewUtil
 import me.ranko.autodark.databinding.MainActivityBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: MainActivityBinding
+
+    /**
+     * Show this dialog if boot receiver is disabled
+     *
+     * @see     checkBootReceiver
+     * */
+    private var receiverDialog: AlertDialog? = null
 
     private val summaryTextListener = object : Observable.OnPropertyChangedCallback() {
         override fun onPropertyChanged(sender: Observable, propertyId: Int) {
@@ -54,6 +65,35 @@ class MainActivity : AppCompatActivity() {
         } else {
             ViewUtil.setImmersiveNavBar(window)
         }
+
+        checkBootReceiver()
+    }
+
+    /**
+     * Some optimize app or OEM performance boost function can disable boot receiver
+     * Notify user if this happened
+     *
+     * */
+    private fun checkBootReceiver() {
+        if (!ComponentUtil.isEnabled(this, DarkModeAlarmReceiver::class.java)) {
+            Timber.e("checkBootReceiver: receiver disabled!")
+            if (receiverDialog == null) {
+                receiverDialog = AlertDialog.Builder(this)
+                    .setTitle(R.string.app_receiver_disabled_title)
+                    .setMessage(R.string.app_receiver_disabled_msg)
+                    .setPositiveButton(R.string.app_confirm) { _, _ -> finish() }
+                    .create()
+            }
+
+            receiverDialog!!.show()
+        } else {
+            Timber.d("checkBootReceiver: receiver enabled")
+        }
+    }
+
+    override fun onStop() {
+        receiverDialog?.run { if (isShowing) dismiss() }
+        super.onStop()
     }
 
     override fun onDestroy() {
