b3a4dd9866a6807efe1f12a0c71692f0912cf107
0ranko0P <ranko0p@outlook.com>
2020-01-30 18:18:36 +0800

    Add ShizukuApi

--- app/src/main/java/me/ranko/autodark/AutoDarkApplication.java
+++ app/src/main/java/me/ranko/autodark/AutoDarkApplication.java
@@ -1,12 +1,21 @@
 package me.ranko.autodark;
 
 import android.app.Application;
+import android.content.Context;
 
 import me.ranko.autodark.core.DebugTree;
 import me.ranko.autodark.core.ReleaseTree;
+import moe.shizuku.api.ShizukuClientHelper;
+import moe.shizuku.api.ShizukuService;
 import timber.log.Timber;
 
 public final class AutoDarkApplication extends Application {
+
+    private static boolean v3Failed;
+
+    public static boolean isShizukuV3Failed() {
+        return v3Failed;
+    }
 
     @Override
     public void onCreate() {
@@ -16,5 +25,28 @@ public final class AutoDarkApplication extends Application {
         } else {
             Timber.plant(ReleaseTree.INSTANCE);
         }
+    }
+
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        ShizukuClientHelper.setBinderReceivedListener(() -> {
+            if (ShizukuService.getBinder() == null) {
+                // ShizukuBinderReceiveProvider started without binder, should never happened
+                Timber.d("binder is null");
+                v3Failed = true;
+            } else {
+                try {
+                    // test the binder first
+                    ShizukuService.pingBinder();
+                } catch (Throwable tr) {
+                    // blocked by SELinux or server dead, should never happened
+                    Timber.i("can't contact with remote", tr);
+                    v3Failed = true;
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/me/ranko/autodark/core/ShizukuApi.kt
@@ -0,0 +1,63 @@
+package me.ranko.autodark.core
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import me.ranko.autodark.AutoDarkApplication.isShizukuV3Failed
+import me.ranko.autodark.Exception.CommandExecuteError
+import me.ranko.autodark.Utils.ShellJobUtil
+import moe.shizuku.api.ShizukuService
+import java.lang.Exception
+import java.lang.IllegalArgumentException
+
+object ShizukuApi {
+
+    suspend fun checkShizuku(): Boolean {
+        // Shizuku v3 service will send binder via Content Provider to this process when this activity comes foreground.
+
+        // Wait a few seconds here for binder
+
+        delay(2000L)
+        return if (!ShizukuService.pingBinder()) {
+            if (isShizukuV3Failed()) {
+                // provider started with no binder included, binder calls blocked by SELinux or server dead, should never happened
+                // notify user
+            }
+
+            // Shizuku v3 may not running, notify user
+            false
+        } else {
+            // Shizuku v3 binder received
+            true
+        }
+    }
+
+    suspend fun runShizukuShell(vararg commands: String) {
+        runShizukuShellForValue(*commands)
+    }
+
+    suspend fun runShizukuShellForValue(vararg commands: String): String? = withContext(Dispatchers.IO) {
+        if(commands.isEmpty()) throw IllegalArgumentException("No commands to execute!")
+
+        var process: Process? = null
+        try {
+            process = ShizukuService.newProcess(arrayOf("sh"), null, null)
+            process.outputStream.use {
+                commands.forEach {command ->
+                    it.write("$command\n".toByteArray())
+                }
+                it.write("exit\n".toByteArray())
+            }
+
+            val result = ShellJobUtil.readStdout(process.inputStream, true)
+            if (process.waitFor() != 0)
+                throw CommandExecuteError(ShellJobUtil.readStdout(process.errorStream, false))
+
+            return@withContext result
+        } catch (e: Exception) {
+            throw CommandExecuteError(e)
+        } finally {
+            process?.destroy()
+        }
+    }
+}
