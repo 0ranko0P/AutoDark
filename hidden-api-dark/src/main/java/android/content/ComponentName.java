51b82927df08780e9580f4a7b726860715de7ae2
0ranko0P <ranko0p@outlook.com>
2021-03-15 20:55:59 +0800

    Fix missing method

--- hidden-api-dark/src/main/java/android/content/ComponentName.java
+++ hidden-api-dark/src/main/java/android/content/ComponentName.java
@@ -8,4 +8,12 @@ public final class ComponentName  {
     public ComponentName(@NonNull String pkg, @NonNull String cls) {
         throw new RuntimeException("STUB");
     }
+
+    public @NonNull String getPackageName() {
+        throw new RuntimeException("STUB");
+    }
+
+    public @NonNull String getClassName() {
+        throw new RuntimeException("STUB");
+    }
 }
